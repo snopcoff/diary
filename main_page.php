9c4bf1e747177cc55c3dd8b296d7546867bcb13e
snopcoff <snoopy.8994@gmail.com>
2015-11-11 11:34:05 +0700

    Update main_page

--- main_page.php
+++ main_page.php
@@ -10,21 +10,30 @@ and open the template in the editor.
         <title>Main Page</title>
         <link rel="stylesheet" type="text/css" href="main_page_style.css" />
     </head>
-    <body>
-    	<ul>
-    		<li class="logo"><input class="button" type="button" onclick="location.href='main_page.php'" /></li>
-    		<li class="search">
-    			<form action="search.php" method="GET" name="search">
-    				<input type="text" name="search" size="30" />
-    				<input type="submit" value="SEARCH" />
-    			</form>
-    		</li>
-    	</ul>
-    	<div id="header">
-    		
-    	</div>
+    <body>    	
+    	<nav>
+    		<ul>
+    			<li class="logo"><input class="button" type="button" onclick="location.href='main_page.php'" /></li>
+    			<li class="search">
+    				<form action="search.php" method="GET" name="search">
+    					<input type="text" name="search" size="50" />
+    					<input class="sbutton" type="submit" value="SEARCH" />
+    				</form>
+    			</li>
+    		</ul>
+    	</nav>
     	<div id="content">
-    		
+    		<div class="content_post">
+    			<p>This is the post
+    			s;fjsldfj;asljf;slkdfj
+    			dfslkafjds;lf;sldjf
+    			sdflksdf;sadjflsdkjf;lsdf
+    			asfds;dfj</p>
+    			<p>;jksdf;lsadjf;lasdjfkdsf
+    			dfla;sdflsdfjk;laskjfa
+    			fdsdlkfj;saljfs;ldjkf;af
+    			sadflk;asfj</p>
+    		</div>
     	</div>
     	<div id="footer">
     		
